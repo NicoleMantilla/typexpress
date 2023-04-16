f2dd4b803ca166e2d613e4fbf40df58e97ff1d4e
Nicole Mantilla <nicolemf14@gmail.com>
2023-04-16 21:21:52 +0200

    css con react.

--- front_react/src/App.js
+++ front_react/src/App.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
 
+
+
+
 function App() {
   const [nombre, setNombre] = useState( "Nombre Producto");
 
@@ -17,17 +20,33 @@ function App() {
   
 
   return (
-    <div classNAme= "App">
-      <h1> Item </h1>
-      <label > Name </label>
-      <input id= "nombre" name= "nombre" value= { nombre } onChange={cambiarNombre}></input>
-      <label for="description"> Description</label>
-      <input id= "description" name= "description" type= "text" placeholder= "Add description"></input>
-      <label> Price</label>
-      <input type="number" id="price" name="price" pattern="[0-9]+" placeholder="price"></input>
-      <button onClick={guardarClick}>Subir</button>
+    <div className= "form_container">
+        <div className="form_header"> 
+          <h1 className= "form_title"> Formulario de productos</h1>
+        </div>
+      <div className="form_body">
 
+      <div className='form_form'>
 
+          <label className='form_labelName' > Name </label>
+          <input className= "form_input" id= "nombre" name= "nombre" value= { nombre } onChange={cambiarNombre}></input>
+          <label for="description"> Description</label>
+          <input id= "description" name= "description" type= "text" placeholder= "Add description"></input>
+          <label> Price</label>
+          <input type="number" id="price" name="price" pattern="[0-9]+" placeholder="price"></input>
+          <button className="form_button" onClick={guardarClick}>Subir</button>
+          <i className="fa-thin fa-upload"></i>
+    </div>
+
+  </div>
+      
+    <div className='form_footer'>
+      <div className ="form_group"> </div>
+    
+    
+    </div>
+
+      
     </div>
   );
 
